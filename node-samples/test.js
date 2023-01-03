80d5d412a1d60a55d64c4f4bfb1082af9bae5070
Clark Lee <saintfeng@users.noreply.github.com>
2023-01-03 17:14:43 +0800

    Update test.js

--- node-samples/test.js
+++ node-samples/test.js
@@ -5,7 +5,8 @@ var lib = ffi.Library(path.join(__dirname, 'libmpc_hd_gg18.so'), {
 	    'keygen': [ 'void', ['string','string','string','string']],
 	    'sign': ['void', ['string','string','string','string','string','string']],
 	    'keygendun': [ 'void', ['string','string','string','string','string','string']],
-	    'keygendunnew': [ 'void', ['string','string','string','string','string','string']]
+	    'keygendunnew': [ 'void', ['string','string','string','string','string','string']],
+	    'pubkey': [ 'void', ['string','string']],
 });
 
 function sign(url, keyfile, config, message, code) {
@@ -16,11 +17,14 @@ function sign(url, keyfile, config, message, code) {
 function keygendun(url, keyfile, config, code) {
     lib.keygendun(url, keyfile, config, code, '', '');
 }
+function keygendun5(url, keyfile, config, code, phrase) {
+    lib.keygendun(url, keyfile, config, code, phrase, '');
+}
+
 
 function keygendunnew(url, keyfile, config, code) {
     lib.keygendunnew(url, keyfile, config, code, '', '');
 }
-
 
 var cmd = process.argv[2];
 var args = process.argv.slice(3);
@@ -44,9 +48,14 @@ else if (cmd === 'keygendunnew') {
 else if (cmd === 'keygendun') {
     if (process.argv.length - 3 === 4) {
         keygendun(...args);
+    } else if (process.argv.length - 3 === 5) {
+        keygendun5(...args);
     } else {
         lib.keygendun(...args);
     }
+}
+else if (cmd === 'pubkey') {
+    lib.pubkey(...args);
 }
 else {
     console.log(`Interface ${cmd} not yet supported.`);
